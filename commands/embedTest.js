fd557a9ce049bc4aa33b9ffb6673b4b4276d7f8f
gads2 <gwendolen@dennis-sellers.com>
2023-03-03 20:07:33 +0000

    Added JSON handling scripts

--- commands/embedTest.js
+++ commands/embedTest.js
@@ -21,8 +21,10 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor(0x084DA5)
             .setTitle("Lucy Cavendish Film Society Film List")
-            .setDescription("Here is a list of lucy cav movies woah.")
+            .setDescription("Here is a list of lucy cav movies woah.\n This is so cool and swag \n this is going to be interesting to format i think \n   **-**   Boop") 
             .setFooter({text: "Page 1/x"});
+
+        //okay so just need a function in as separate script to read in the JSON and then convert it to correct string formatting with the \n and **-** business and then we good I think?
 
         await interaction.reply({embeds: [embed], components: [row]});
     }
